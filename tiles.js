245c86a94c0d56196d0259eb07bd074a2c33a692
canfeit <canfeit@qq.com>
2019-02-01 16:56:42 +0800

    update tiles

--- tiles.js
+++ tiles.js
@@ -1,32 +1,35 @@
-// $.StartScreen = function() {
-//   // var plugin = this;
-//   // var width = window.innerWidth > 0 ? window.innerWidth : screen.width;
-//   // plugin.init = function() {
-//   //   // setTilesAreaSize();
-//   //   // if (width > Metro.media_sizes.LD) {
-//   //   //   $(".start-screen").css({
-//   //   //     overflow: "auto"
-//   //   //   });
-//   //   // }
-//   // };
-//   // var setTilesAreaSize = function() {
-//   //   var groups = $(".tiles-group");
-//   //   var tileAreaWidth = 80;
-//   //   $.each(groups, function(i, t) {
-//   //     if (width <= Metro.media_sizes.LD) {
-//   //       tileAreaWidth = width;
-//   //     } else {
-//   //       tileAreaWidth += $(t).outerWidth() + 80;
-//   //     }
-//   //   });
-//   //   $(".tiles-area").css({
-//   //     width: tileAreaWidth
-//   //   });
-//   // };
-//   // plugin.init();
-// };
+$.StartScreen = function() {
+  var plugin = this;
+  var width = window.innerWidth > 0 ? window.innerWidth : screen.width;
 
-// $.StartScreen();
+  plugin.init = function() {
+    setTilesAreaSize();
+    if (width > Metro.media_sizes.LD) {
+      $(".start-screen").css({
+        overflow: "auto"
+      });
+    }
+  };
+
+  var setTilesAreaSize = function() {
+    var groups = $(".tiles-group");
+    var tileAreaWidth = 80;
+    $.each(groups, function(i, t) {
+      if (width <= Metro.media_sizes.LD) {
+        tileAreaWidth = width;
+      } else {
+        tileAreaWidth += $(t).outerWidth() + 80;
+      }
+    });
+    $(".tiles-area").css({
+      width: tileAreaWidth
+    });
+  };
+
+  plugin.init();
+};
+
+$.StartScreen();
 
 $.each($("[class*=tile-]"), function() {
   var tile = $(this);
@@ -45,6 +48,6 @@ $(".tiles-group").animate({
   left: 0
 });
 
-// $(window).on(Metro.events.resize + "-start-screen-resize", function() {
-//   // $.StartScreen();
-// });
+$(window).on(Metro.events.resize + "-start-screen-resize", function() {
+  $.StartScreen();
+});
