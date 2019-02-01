ab1b45f247166c51a447940909c51ffdb0fd0c1f
Wang Jianhui <canfeit@qq.com>
2019-02-01 11:11:38 +0000

    Update desktop.js

--- desktop.js
+++ desktop.js
@@ -6,29 +6,11 @@ var Desktop = {
     taskBarClass: ""
   },
 
-  wins: {},
-
   createWindow: function(o) {
     var that = this;
-//    o.onDragStart = function(pos, el) {
- //     win = $(el);
- //     $(".window").css("z-index", 1);
- //     if (!win.hasClass("modal")) win.css("z-index", 3);
-  //  };
-    o.onDragStop = function(pos, el) {
-   //   win = $(el);
-  //    if (!win.hasClass("modal")) win.css("z-index", 2);
-    };
-    o.onWindowDestroy = function(win) {
- //     that.removeFromTaskBar(win);
-    };
     var w = $("<div>").appendTo($(this.options.windowArea));
-    var wnd = w.window(o).data("window");
-
-    var win = wnd.win;
+    var wnd = $("body").window(o).data("window");
    
     w.remove();
   }
 };
-
-// Desktop.setup();
