ff22f107b084b3d136045ed994a2c5a0b536c9fb
canfeit <canfeit@qq.com>
2019-02-01 16:25:04 +0800

    update tiles

--- tiles.js
+++ tiles.js
@@ -1,46 +1,32 @@
-$.StartScreen = function() {
-  var plugin = this;
-  var width = window.innerWidth > 0 ? window.innerWidth : screen.width;
+// $.StartScreen = function() {
+//   // var plugin = this;
+//   // var width = window.innerWidth > 0 ? window.innerWidth : screen.width;
+//   // plugin.init = function() {
+//   //   // setTilesAreaSize();
+//   //   // if (width > Metro.media_sizes.LD) {
+//   //   //   $(".start-screen").css({
+//   //   //     overflow: "auto"
+//   //   //   });
+//   //   // }
+//   // };
+//   // var setTilesAreaSize = function() {
+//   //   var groups = $(".tiles-group");
+//   //   var tileAreaWidth = 80;
+//   //   $.each(groups, function(i, t) {
+//   //     if (width <= Metro.media_sizes.LD) {
+//   //       tileAreaWidth = width;
+//   //     } else {
+//   //       tileAreaWidth += $(t).outerWidth() + 80;
+//   //     }
+//   //   });
+//   //   $(".tiles-area").css({
+//   //     width: tileAreaWidth
+//   //   });
+//   // };
+//   // plugin.init();
+// };
 
-  plugin.init = function() {
-    setTilesAreaSize();
-    if (width > Metro.media_sizes.LD) {
-      addMouseWheel();
-      $(".start-screen").css({
-        overflow: "auto"
-      });
-    }
-  };
-
-  var setTilesAreaSize = function() {
-    var groups = $(".tiles-group");
-    var tileAreaWidth = 80;
-    $.each(groups, function(i, t) {
-      if (width <= Metro.media_sizes.LD) {
-        tileAreaWidth = width;
-      } else {
-        tileAreaWidth += $(t).outerWidth() + 80;
-      }
-    });
-    $(".tiles-area").css({
-      width: tileAreaWidth
-    });
-  };
-
-  var addMouseWheel = function() {
-    $("body").mousewheel(function(event, delta, deltaX, deltaY) {
-      var page = $(".start-screen");
-      var scroll_value = delta * 50;
-      console.log(scroll_value);
-      page.scrollLeft(page.scrollLeft() - scroll_value);
-      return false;
-    });
-  };
-
-  plugin.init();
-};
-
-$.StartScreen();
+// $.StartScreen();
 
 $.each($("[class*=tile-]"), function() {
   var tile = $(this);
@@ -59,6 +45,6 @@ $(".tiles-group").animate({
   left: 0
 });
 
-$(window).on(Metro.events.resize + "-start-screen-resize", function() {
-  $.StartScreen();
-});
+// $(window).on(Metro.events.resize + "-start-screen-resize", function() {
+//   // $.StartScreen();
+// });
